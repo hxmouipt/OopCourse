29f6a3b646980297dbd598c24b22144a253b677a
unknown <hxmouipt@gmail.com>
2015-04-30 08:42:46 -0700

    java programm from beginning until 20150430

---
+++ TestVar.java
@@ -0,0 +1,20 @@
+public class TestVar {
+	static int j;
+	public void m() {
+		int i = 0;
+		System.out.println(i);
+	}
+
+	public static void main(String[] args) {
+		int i = 0;
+		System.out.println(i);
+		System.out.println(j);
+
+		boolean b = false;
+		if(b){
+			System.out.println("b is true");
+		}
+
+		long longNum1 = 8888888;
+	}
+}
