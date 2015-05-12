fff3eabba918d92b6f0c52fac030027ee37a5cc9
unknown <hxmouipt@gmail.com>
2015-05-12 11:37:30 -0700

    2015-05-12 update

---
+++ TestPoint.java
@@ -0,0 +1,38 @@
+class Pointxyz {
+	double x, y, z;
+	Pointxyz(double _x, double _y, double _z) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+	
+	void setX(double _x) {
+		x = _x;
+	}
+
+	void setY(double _y) {
+		y = _y;
+	}
+
+	void setZ(double _z) {
+		z = _z;
+	}
+
+	double getDistance(Pointxyz p) {
+		return  (x-p.x) * (x-p.x) + (y-p.y)* (y-p.y) + (z-p.z) * (z-p.z);
+	}
+}
+
+
+
+	public class TestPoint {
+		public static void main (String[] args) {
+			Pointxyz p = new Pointxyz(1.0, 2.0, 3.0);		
+			Pointxyz p1 = new Pointxyz(0.0, 0.0, 0.0);
+			System.out.println(p.getDistance(p1));
+		p.setX(5.0);
+		System.out.println(p.getDistance(new Pointxyz(1.0, 1.0, 1.0)));
+	} 
+}
+
+
